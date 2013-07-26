8e90b00a1ffb3525f476bcb7c50c2983ce6d7b07
Alex Lin <alexander.lin.kremer@gmail.com>
2013-07-26 15:50:59 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TowerOfBabel::Application.config.secret_token = 'c8848bd812e1dd066182efa5f357918f605eda6faaa37ce3c6858b7d003eda024d6ed5118e523d1ded9985fc66f9a37476b66fac686a766d3c98c7177e869949'
