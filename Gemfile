21dfff5ee4588650db7d352853f83e16e8c48de6
Alex Lin <alexander.lin.kremer@gmail.com>
2013-07-28 03:35:39 -0700

    added locations

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.2.13'
 gem 'pg'
 
 gem 'sorcery'
+gem 'geocoder'
 
 
 group :development do
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   has_many :messages
 
-  attr_accessible :username, :email, :password, :password_confirmation
+  attr_accessible :username, :email, :password, :password_confirmation, :address, :latitude, :longitude
 
   validates_length_of :password, :minimum => 3, :message => "password must be at least 3 characters long", :if => :password
   validates_confirmation_of :password, :message => "should match confirmation", :if => :password
